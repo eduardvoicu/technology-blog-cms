7498455a4d34c42a6d2aeae26cfaab2f458e5694
Eduard Voicu <iameduardvoicu@gmail.com>
2021-12-06 18:46:22 -0600

    server.js complete

--- server.js
+++ server.js
@@ -18,3 +18,27 @@ const sess = {
     db: sequelize
   })
 };
+
+app.use(session(sess));
+
+const hbs = exphbs.create({
+  helpers: {
+    format_date: date => {
+      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    }
+  }
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require('./controllers/'));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
+});
